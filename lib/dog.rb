6ff5490945a82e1439437e83d6a682b8193d3b8a
kevin murphy <kevinnealmurphy@gmail.com>
2020-05-08 21:08:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,9 +4,25 @@ class Dog
   @@all = []
   
   def initialize
-    @@all << name
-    @name
+    @name = name
+    save
+
   end
   
+  def self.all
+    @all
+  end
+  
+  def self.print_all
+    @all.each {|i| puts "#{i.name}"
+  end 
+  
+  def save
+    @@all << self
+  end
+  
+  def self.clear_all
+    
+  end
   
 end
